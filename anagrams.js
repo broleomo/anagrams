73e9b7cf25cdc65fb9377a9603217a11e9396555
broleomo <brooklynnmoor@gmail.com>
2017-11-03 15:52:48 -0400

    first problem finished

---
+++ anagrams.js
@@ -0,0 +1,29 @@
+// eventually will change this to an input value for a search of a word e.target.value
+
+// p1.
+let givenWord = "least";
+function alphabetize(word){
+  return word.split('').sort().join('');
+}
+let matches = words.filter(function(word){
+  return alphabetize(word) == alphabetize(givenWord);
+});
+
+// p2.
+
+let alphabetizedList = alphabetize(words);
+
+// create a loop using the length of the entire words list and give each word a value of [the variations of anagrams]
+let dictionary = {}
+for (var i = 0; i < words.length; i++) {
+  let word = words[i]
+  let matches = words.filter(function(word){
+    return alphabetize(word) == alphabetize(word);
+  }
+  dictionary.push = {word[i]: [matches]}
+}
+
+
+// words.forEach(function(item,index){
+//   if(index % 2 === 0)
+// })
