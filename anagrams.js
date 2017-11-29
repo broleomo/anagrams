fd233eba89255a2a7496f5093913d4dc97beba7a
broleomo <brooklynnmoor@gmail.com>
2017-11-29 14:47:09 -0500

    finished p4

--- anagrams.js
+++ anagrams.js
@@ -1,20 +1,23 @@
 // eventually will change this to an input value for a search of a word e.target.value
 // p1. & p.3
 function searchWord(){
-let givenWord = document.getElementById('entry').value;
+let givenWord = document.getElementById('entry');
+let input2 = alphabetize(givenWord.value);
+console.log(input2);
 let matches = words.filter(function(word){
   return alphabetize(word) == alphabetize(givenWord);
 });
 console.log(matches);
 }
-function alphabetize(word){
-  return word.split('').sort().join('');
+
+function alphabetize(str){
+  return str.toLowerCase().split('').sort().join('').trim();
 }
 
 // p2.
 // create a loop using the length of the entire words list and give each word a value of [the variations of anagrams]
-function makeDictionary() {
   let dictionary = {}
+function makeDictionary() {
   for (var i = 0; i < words.length; i++) {
     let givenWord = words[i]
     let alphabetizedWord = alphabetize(givenWord);
@@ -41,3 +44,23 @@ function makeFiveDictionary(){
 console.log(makeFiveDictionary());
 
 // p.4
+// doesInclude  - use dictionary to search that the letters are included
+// write helper function to subtract letters from input string
+// first, create helper function for subtractLetters()
+// then create a loop for all the words in the dictionary
+
+let array = Object.keys(dictionary)
+
+function handleWord(){
+let results = [];
+let givenWord = alphabetize(document.getElementById('entry').value);
+for(let i=0; i<array.length -1 ; i++){
+  for(let j=i+1; j<array.length; j++){
+    if(givenWord.length ===((array[i]+ array[j]).length)){
+      if (alphabetize(array[i] + array[j]) === givenWord) {
+          console.log(dictionary[array[i]] , dictionary[array[j]]);
+      }
+    }
+  }
+}
+}
